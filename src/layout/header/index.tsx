b6b344589c7eed9bf7c8c5dd81ab43fe51a6e8ea
nastaran-shafie <nastaran.sh33@gmail.com>
2023-04-18 16:57:41 +0430

    feat:create header layout

---
+++ src/layout/header/index.tsx
@@ -0,0 +1,39 @@
+import bag from "../../../public/image/bag.png";
+import ButtonComponent from "../../component/button";
+import { Icon } from "@iconify/react";
+function HeaderPage() {
+  return (
+    <header className="w-full justify-center gap-3 lg:w-[85.375rem] h-[6.375rem] flex lg:justify-around items-center lg:border">
+      <div className="flex gap-2 items-center">
+        <img src={bag} alt="" />
+        <p className="hidden lg:block text-orange-600 text-lg">ECOshop</p>
+      </div>
+      <div className="flex gap-2 lg:gap-8 lg:mr-40">
+        <p>electronic</p>
+        <p>women</p>
+        <p>men</p>
+        <p>jewelry</p>
+      </div>
+      <div className="flex gap-4">
+        <div className="w-[2.7rem] h-[2.7rem] bg-button flex justify-center items-center rounded-[0.4rem]">
+          <Icon
+            icon="solar:cart-3-bold"
+            width="30"
+            height="30"
+            className="text-white"
+          />
+        </div>
+        <div className="w-[2.7rem] h-[2.7rem] bg-button flex justify-center items-center rounded-[0.4rem] lg:hidden">
+          <Icon
+            icon="material-symbols:add-circle-outline"
+            width="30"
+            height="30"
+            className="text-white"
+          />
+        </div>
+        <ButtonComponent title="AddProduct" className="hidden lg:block" />
+      </div>
+    </header>
+  );
+}
+export default HeaderPage;
--- src/rootLayout/index.tsx
+++ src/rootLayout/index.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
+import HeaderPage from "../layout/header";
 function SiteLayout() {
   return (
     <>
+      <HeaderPage />
       <Outlet></Outlet>
     </>
   );
