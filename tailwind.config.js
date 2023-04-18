4880aa1a8dee19598c09ef4f8c2f56835cbb801f
nastaran-shafie <nastaran.sh33@gmail.com>
2023-04-18 15:54:18 +0430

    feat:add style for lgin page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+import bg from "../ECOshop/public/image/bg 2.png";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgLogin: "#3E18D9",
+        purpleC: "#6F11E1",
+      },
+      backgroundImage: {
+        "hero-lg": "url('../ECOshop/public/image/bg 2.png')",
+      },
+    },
   },
   plugins: [],
 };
