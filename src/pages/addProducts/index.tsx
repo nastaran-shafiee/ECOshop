5cf62b400bcc3d40ad46e8a61b2a328594a71541
nastaran-shafie <nastaran.sh33@gmail.com>
2023-04-22 02:04:43 +0430

    feat: add custom hook useadd for add product page

--- src/pages/addProducts/index.tsx
+++ src/pages/addProducts/index.tsx
@@ -1,10 +1,15 @@
 import ButtonComponent from "../../component/button";
 import Input from "../../component/input";
+import useAdd from "../../hooks/useAdd";
 
 function AddProductsPage() {
+  const { register, handleSubmit, errors, handleLoginUser } = useAdd();
   return (
     <div className="flex justify-center">
-      <div className="flex w-[90%] lg:w-[35rem] bg-white flex-col items-center mb-8">
+      <form
+        className="flex w-[90%] lg:w-[35rem] bg-white flex-col items-center mb-8"
+        onSubmit={handleSubmit(handleLoginUser)}
+      >
         <div className="bg-white mt-8 mb-4 text-grayC ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
           alias eaque voluptatum consequatur saepe, architecto sint eligendi,
@@ -15,30 +20,40 @@ function AddProductsPage() {
         <div className="bg-table2  w-full flex items-center pt-16 flex-col gap-8 ">
           <Input
             text="title"
+            error={errors.title?.message}
+            validation={{ ...register("title") }}
             className=" bg-table2"
             classname2=" !bg-table2 "
             clasname3="w-[266px] h-[42px] lg:w-[22.4rem] "
           />
           <Input
             text="Description"
+            error={errors.Description?.message}
+            validation={{ ...register("Description") }}
             className="bg-table2"
             classname2=" !bg-table2 "
             clasname3="w-[266px] h-[42px] lg:w-[22.4rem] "
           />
           <Input
             text="category"
+            error={errors.category?.message}
+            validation={{ ...register("category") }}
             className="bg-table2"
             classname2=" !bg-table2 "
             clasname3="w-[266px] h-[42px] lg:w-[22.4rem] "
           />
           <Input
             text="Image URL"
+            error={errors.ImageURL?.message}
+            validation={{ ...register("ImageURL") }}
             className="bg-table2"
             classname2=" !bg-table2 "
             clasname3="w-[266px] h-[42px] lg:w-[22.4rem] "
           />
           <Input
             text="price"
+            error={errors.price?.message}
+            validation={{ ...register("price") }}
             className="bg-table2"
             classname2=" !bg-table2 "
             clasname3="w-[266px] h-[42px] lg:w-[22.4rem] "
@@ -51,10 +66,11 @@ function AddProductsPage() {
           />
           <ButtonComponent
             title="Add Product"
+            type="submit"
             className="bg-orange-600  w-[8rem] lg:w-[10.6rem]"
           />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
